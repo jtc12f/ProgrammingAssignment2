184111877f8e5fb88f2ea5aa558e4d4f2961c614
jtc12f <jamestcochran@gmail.com>
2017-12-10 15:31:02 -0600

    changes for assignment
    added code to complete assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## these functions work together to store inverted matrices
+## in the environment of the function, also include functions that
+## call and retrieve the inverted matrices
 
-## Write a short comment describing this function
+## makeCacheMatrix takes an invertible matrix (x) and stores it as an
+## object that caches its inverse. It also contains functions that can set
+## and get the matrices and their inverses
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL ## default value for inverse is null
+        set <- function(y){
+                x <<-y
+                inv <- NULL
+        }
+        get <- function() x ## value of x is taken from parent environment
+        set_inverse <- function(inverse) inv <<- inverse
+        get_inverse <- function() inv 
+        list(set = set, get = get, set_inverse = set_inverse, get_inverse = get_inverse)
+        
 }
 
 
-## Write a short comment describing this function
+## cacheSolve performs the inversion of the matrix from makeCacheMatrix.
+## if already computed, will pull from cache. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+       inv <- x$get_inverse()   
+       if(!is.null(inv)){       ##evaluates whether inverse has been computed
+               message("retrieving inverted matrix from cache") 
+               return(inv) ##if so, returns the inverted matrix without computing again
+       }
+       
+       ## if inv is NULL (not yet calculated), proceeds to calculate inverse
+       
+       input_matrix <- x$get() 
+       inv <- solve(input_matrix,...) ## calculates inverse of input_matrix
+       
+       x$set_inverse(inv) ## sets the inverse in the cache using set_inverse
+       inv ## prints inverse
 }
